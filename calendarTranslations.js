2a98bda3eb5f86428e649c6d748de9e9753b199b
Adam Nathaniel Davis <adam.davis@hashverify.com>
2020-02-04 11:42:34 -0500

    Create calendarTranslations.js

---
+++ calendarTranslations.js
@@ -0,0 +1,31 @@
+const calendarTranslations = {
+	1 : { // english
+		"April" : "April",
+		"August" : "August",
+		"December" : "December",
+		"February" : "February",
+		"January" : "January",
+		"June" : "June",
+		"July" : "July",
+		"March" : "March",
+		"May" : "May",
+		"November" : "November",
+		"October" : "October",
+		"September" : "September",
+	},
+	2 : { // spanish
+		"April" : "Abril",
+		"August" : "Agosto",
+		"December" : "Diciembre",
+		"February" : "Febrero",
+		"January" : "Enero",
+		"July" : "Julio",
+		"June" : "Junio",
+		"March" : "Marzo",
+		"May" : "Mayo",
+		"November" : "Noviembre",
+		"October" : "Octubre",
+		"September" : "Septiembre",
+	},
+};
+export default calendarTranslations;
